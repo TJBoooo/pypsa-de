3424ef0695d62e1fab34248c843f29081dc5a872
Michael Lindner <michaellindner@posteo.de>
2026-02-23 13:41:06 +0100

    add a delay for offshore projects in 2020-2025 to correspond better to real capacities

--- scripts/pypsa-de/modify_prenetwork.py
+++ scripts/pypsa-de/modify_prenetwork.py
@@ -1189,6 +1189,7 @@ def force_connection_nep_offshore(n, current_year, costs):
         offshore.loc[offshore["Inbetriebnahmejahr"] > 2025, "Inbetriebnahmejahr"] += (
             int(snakemake.params.offshore_nep_force["delay_years"])
         )
+        offshore.loc[offshore["Inbetriebnahmejahr"] <= 2025, "Inbetriebnahmejahr"] += 1
         logger.info(
             f"Delaying NEP offshore connection points by {snakemake.params.offshore_nep_force['delay_years']} years."
         )
